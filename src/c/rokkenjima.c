a1c1ced46d0f26dd7296a3e9e0ac5124eef96b56
Jen Tran <jen.tran@uwaterloo.ca>
2016-10-29 00:42:15 -0400

    Added minute and hour hands

--- src/c/rokkenjima.c
+++ src/c/rokkenjima.c
@@ -1,18 +1,18 @@
 #include <pebble.h>
+#include "rokkenjima.h"
 
 static Window *s_window;
-static Layer *s_canvas_layer;
-
-static void update_time() {
-  time_t temp = time(NULL);
-  struct tm *tick_time = localtime(&temp);
-}
+static Layer *s_canvas_layer, *s_hands_layer;
+static GPath *s_tick_paths[NUM_CLOCK_TICKS];
+static GPath *s_minute_arrow, *s_hour_arrow;
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
-  update_time();
+  layer_mark_dirty(window_get_root_layer(s_window));
 }
 
 static void bluetooth_callback(bool connected) {
+  window_set_background_color(s_window, GColorBlack);
+
   if (!connected) {
     vibes_double_pulse();
   }
@@ -33,16 +33,38 @@ static void canvas_update_proc(Layer *layer, GContext *ctx) {
   graphics_draw_circle(ctx, centre, radius-4);
   graphics_draw_circle(ctx, centre, radius-6);
   graphics_draw_circle(ctx, centre, radius/3);
-
+        
   APP_LOG(APP_LOG_LEVEL_DEBUG, "Drawn canvas");
+}
+
+static void hands_update_proc(Layer *layer, GContext *ctx) {
+  GRect bounds = layer_get_bounds(layer);
+
+  time_t now = time(NULL);
+  struct tm *t = localtime(&now);
+
+  // minute/hour hand
+  graphics_context_set_fill_color(ctx, GColorYellow);
+  graphics_context_set_stroke_color(ctx, GColorBlack);
+
+  gpath_rotate_to(s_minute_arrow, TRIG_MAX_ANGLE * t->tm_min / 60);
+  gpath_draw_filled(ctx, s_minute_arrow);
+  gpath_draw_outline(ctx, s_minute_arrow);
+
+  gpath_rotate_to(s_hour_arrow, (TRIG_MAX_ANGLE * (((t->tm_hour % 12) * 6) + (t->tm_min / 10))) / (12 * 6));
+  gpath_draw_filled(ctx, s_hour_arrow);
+  gpath_draw_outline(ctx, s_hour_arrow);
+
+  // dot in the middle
+  graphics_context_set_fill_color(ctx, GColorBlack);
+  graphics_fill_rect(ctx, GRect(bounds.size.w / 2 - 1, bounds.size.h / 2 - 1, 3, 3), 0, GCornerNone);
 }
 
 static void prv_window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
-  window_set_background_color(window, GColorBlack);
 
-  update_time();
+  window_set_background_color(s_window, GColorRed);
 
   bluetooth_callback(connection_service_peek_pebble_app_connection());
 
@@ -52,9 +74,14 @@ static void prv_window_load(Window *window) {
   layer_add_child(window_layer, s_canvas_layer);
 
   layer_mark_dirty(s_canvas_layer);
+
+  s_hands_layer = layer_create(bounds);
+  layer_set_update_proc(s_hands_layer, hands_update_proc);
+  layer_add_child(window_layer, s_hands_layer);
 }
 
 static void prv_window_unload(Window *window) {
+  layer_destroy(s_hands_layer);
   layer_destroy(s_canvas_layer);
 }
 
@@ -67,6 +94,19 @@ static void prv_init(void) {
   const bool animated = true;
   window_stack_push(s_window, animated);
 
+  s_minute_arrow = gpath_create(&MINUTE_HAND_POINTS);
+  s_hour_arrow = gpath_create(&HOUR_HAND_POINTS);
+
+  Layer *window_layer = window_get_root_layer(s_window);
+  GRect bounds = layer_get_bounds(window_layer);
+  GPoint centre = grect_center_point(&bounds);
+  gpath_move_to(s_minute_arrow, centre);
+  gpath_move_to(s_hour_arrow, centre);
+
+  for (int i=0; i<NUM_CLOCK_TICKS; i++) {
+   s_tick_paths[i] = gpath_create(&ANALOG_BG_POINTS[i]); 
+  }
+
   tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
 
   connection_service_subscribe((ConnectionHandlers) {
@@ -75,6 +115,13 @@ static void prv_init(void) {
 }
 
 static void prv_deinit(void) {
+  gpath_destroy(s_minute_arrow);
+  gpath_destroy(s_hour_arrow);
+
+  for (int i=0; i<NUM_CLOCK_TICKS; i++) {
+    gpath_destroy(s_tick_paths[i]);
+  }
+
   tick_timer_service_unsubscribe();
   window_destroy(s_window);
 }
