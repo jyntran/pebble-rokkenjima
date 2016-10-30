61648b769371b2c8077dd95aea1c9ba10c80c60b
Jen Tran <jen.tran@uwaterloo.ca>
2016-10-30 02:54:22 -0400

    Added PDC hands, some refactoring

--- src/c/rokkenjima.c
+++ src/c/rokkenjima.c
@@ -1,10 +1,10 @@
 #include <pebble.h>
+#include <pdc-transform/pdc-transform.h>
 #include "rokkenjima.h"
 
 static Window *s_window;
 static Layer *s_canvas_layer, *s_hands_layer;
-static GPath *s_tick_paths[NUM_CLOCK_TICKS];
-static GPath *s_minute_arrow, *s_hour_arrow;
+static GDrawCommandImage *s_minute_hand, *s_hour_hand;
 static GFont s_label_font;
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
@@ -25,7 +25,7 @@ static void canvas_update_proc(Layer *layer, GContext *ctx) {
   
   // White clockface
   GPoint centre = GPoint(bounds.size.w/2, bounds.size.h/2);
-  uint16_t radius = bounds.size.w/2 - 6;
+  uint16_t radius = bounds.size.w/2 - 2;
   graphics_context_set_fill_color(ctx, GColorWhite);
   graphics_fill_circle(ctx, centre, radius);
 
@@ -77,17 +77,16 @@ static void hands_update_proc(Layer *layer, GContext *ctx) {
   graphics_context_set_fill_color(ctx, GColorYellow);
   graphics_context_set_stroke_color(ctx, GColorBlack);
 
-  gpath_rotate_to(s_minute_arrow, TRIG_MAX_ANGLE * t->tm_min / 60);
-  gpath_draw_filled(ctx, s_minute_arrow);
-  gpath_draw_outline(ctx, s_minute_arrow);
+  GPoint centre = GPoint(bounds.size.w/2, bounds.size.h/2);
 
-  gpath_rotate_to(s_hour_arrow, (TRIG_MAX_ANGLE * (((t->tm_hour % 12) * 6) + (t->tm_min / 10))) / (12 * 6));
-  gpath_draw_filled(ctx, s_hour_arrow);
-  gpath_draw_outline(ctx, s_hour_arrow);
+  pdc_transform_gdraw_command_image_draw_transformed(ctx, s_minute_hand, GPoint(centre.x-8,centre.y-60), 10, 360 * t->tm_min / 60);
+
+  pdc_transform_gdraw_command_image_draw_transformed(ctx, s_hour_hand, GPoint(centre.x-8,centre.y-50), 10, (360 * (((t->tm_hour % 12) * 6) + (t->tm_min / 10))) / (12 * 6));
 
   // dot in the middle
-  graphics_context_set_fill_color(ctx, GColorBlack);
-  graphics_fill_rect(ctx, GRect(bounds.size.w / 2 - 1, bounds.size.h / 2 - 1, 3, 3), 0, GCornerNone);
+  uint32_t radius = bounds.size.w/2 - 2; 
+  graphics_fill_circle(ctx, centre, radius/12);
+  graphics_draw_circle(ctx, centre, radius/12);
 }
 
 static void prv_window_load(Window *window) {
@@ -96,8 +95,6 @@ static void prv_window_load(Window *window) {
   
   s_label_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_LABEL_12));
   
-  window_set_background_color(s_window, GColorRed);
-
   bluetooth_callback(connection_service_peek_pebble_app_connection());
 
   // Canvas layer
@@ -107,6 +104,7 @@ static void prv_window_load(Window *window) {
 
   layer_mark_dirty(s_canvas_layer);
 
+  // Hands layer
   s_hands_layer = layer_create(bounds);
   layer_set_update_proc(s_hands_layer, hands_update_proc);
   layer_add_child(window_layer, s_hands_layer);
@@ -116,6 +114,8 @@ static void prv_window_unload(Window *window) {
   layer_destroy(s_hands_layer);
   layer_destroy(s_canvas_layer);
   fonts_unload_custom_font(s_label_font);
+  gdraw_command_image_destroy(s_minute_hand);
+  gdraw_command_image_destroy(s_hour_hand);
 }
 
 static void prv_init(void) {
@@ -127,19 +127,9 @@ static void prv_init(void) {
   const bool animated = true;
   window_stack_push(s_window, animated);
 
-  s_minute_arrow = gpath_create(&MINUTE_HAND_POINTS);
-  s_hour_arrow = gpath_create(&HOUR_HAND_POINTS);
-
-  Layer *window_layer = window_get_root_layer(s_window);
-  GRect bounds = layer_get_bounds(window_layer);
-  GPoint centre = grect_center_point(&bounds);
-  gpath_move_to(s_minute_arrow, centre);
-  gpath_move_to(s_hour_arrow, centre);
-
-  for (int i=0; i<NUM_CLOCK_TICKS; i++) {
-   s_tick_paths[i] = gpath_create(&ANALOG_BG_POINTS[i]); 
-  }
-
+  s_minute_hand = gdraw_command_image_create_with_resource(RESOURCE_ID_IMAGE_MINUTE_HAND);
+  s_hour_hand = gdraw_command_image_create_with_resource(RESOURCE_ID_IMAGE_HOUR_HAND);
+  
   tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
 
   connection_service_subscribe((ConnectionHandlers) {
@@ -148,13 +138,6 @@ static void prv_init(void) {
 }
 
 static void prv_deinit(void) {
-  gpath_destroy(s_minute_arrow);
-  gpath_destroy(s_hour_arrow);
-
-  for (int i=0; i<NUM_CLOCK_TICKS; i++) {
-    gpath_destroy(s_tick_paths[i]);
-  }
-
   tick_timer_service_unsubscribe();
   window_destroy(s_window);
 }
