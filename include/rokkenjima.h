efeca7eeb57c96a337ab1e9ddff091172362a55c
Jen Tran <j.yn.tran@gmail.com>
2016-10-31 21:50:18 -0400

    Set up Clay

--- include/rokkenjima.h
+++ include/rokkenjima.h
@@ -9,3 +9,14 @@ struct Label {
     uint32_t num;
     char text[5];
 };
+
+#define SETTINGS_KEY 1
+
+typedef struct ClaySettings {
+  GColor BackgroundColour;
+  GColor ClockColour;
+  GColor HandColour;
+  GColor HandOutlineColour;
+  bool BtVibration;
+  GColor BtBackgroundColour;
+} ClaySettings;
--- src/c/rokkenjima.c
+++ src/c/rokkenjima.c
@@ -7,23 +7,85 @@ static Layer *s_clock_layer, *s_hands_layer;
 static GDrawCommandImage *s_bg, *s_minute_hand, *s_hour_hand;
 static GFont s_label_font;
 
+struct ClaySettings settings;
+
+static void prv_default_settings() {
+  settings.BackgroundColour = GColorBlack;
+  settings.ClockColour = PBL_IF_COLOR_ELSE(GColorPastelYellow, GColorWhite);
+  settings.HandColour = PBL_IF_COLOR_ELSE(GColorYellow, GColorBlack);
+  settings.HandOutlineColour = PBL_IF_COLOR_ELSE(GColorWindsorTan, GColorBlack);
+  settings.BtVibration = true;
+  settings.BtBackgroundColour = PBL_IF_COLOR_ELSE(GColorRed, GColorWhite);
+}
+
+static void prv_load_settings() {
+  prv_default_settings();
+  persist_read_data(SETTINGS_KEY, &settings, sizeof(settings));
+}
+
+static void prv_update_display() {
+  window_set_background_color(s_window, settings.BackgroundColour);
+  layer_mark_dirty(s_clock_layer);
+  layer_mark_dirty(s_hands_layer);
+}
+
+static void prv_save_settings() {
+  persist_write_data(SETTINGS_KEY, &settings, sizeof(settings));
+  prv_update_display();
+}
+
+static void prv_inbox_received_handler(DictionaryIterator *iter, void *context) {
+  Tuple *bg_colour_t = dict_find(iter, MESSAGE_KEY_BackgroundColour);
+  if (bg_colour_t) {
+    settings.BackgroundColour = GColorFromHEX(bg_colour_t->value->int32);
+  }
+
+  Tuple *ck_colour_t = dict_find(iter, MESSAGE_KEY_ClockColour);
+  if (ck_colour_t) {
+    settings.ClockColour = GColorFromHEX(ck_colour_t->value->int32);
+  }
+
+  Tuple *hd_colour_t = dict_find(iter, MESSAGE_KEY_HandColour);
+  if (hd_colour_t) {
+    settings.HandColour = GColorFromHEX(hd_colour_t->value->int32);
+  }
+
+  Tuple *hdo_colour_t = dict_find(iter, MESSAGE_KEY_HandOutlineColour);
+  if (hdo_colour_t) {
+    settings.HandOutlineColour = GColorFromHEX(hdo_colour_t->value->int32);
+  }
+
+  Tuple *bt_vibe_t = dict_find(iter, MESSAGE_KEY_BtVibration);
+  if (bt_vibe_t) {
+    settings.BtVibration = bt_vibe_t->value->int32 == 1;
+  }
+
+  Tuple *bt_colour_t = dict_find(iter, MESSAGE_KEY_BtBackgroundColour);
+  if (bt_colour_t) {
+    settings.BtBackgroundColour = GColorFromHEX(bt_colour_t->value->int32);
+  }
+
+  prv_save_settings();
+}
+
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
   layer_mark_dirty(window_get_root_layer(s_window));
 }
 
 static void bluetooth_callback(bool connected) {
-  window_set_background_color(s_window, GColorBlack);
+  window_set_background_color(s_window, settings.BackgroundColour);
 
   if (!connected) {
-    window_set_background_color(s_window, PBL_IF_COLOR_ELSE(GColorRed, GColorWhite));
+    window_set_background_color(s_window, settings.BtBackgroundColour);
    
-    // Long vibrate
-    static const uint32_t const segments[] = { 1000, 100, 300 };
-    VibePattern pat = {
-      .durations = segments,
-      .num_segments = ARRAY_LENGTH(segments),
-    };
-    vibes_enqueue_custom_pattern(pat);
+    if (settings.BtVibration) {
+      static const uint32_t const segments[] = { 800, 100, 300 };
+      VibePattern pat = {
+        .durations = segments,
+        .num_segments = ARRAY_LENGTH(segments),
+      };
+      vibes_enqueue_custom_pattern(pat);
+    }
   }
 }
 
@@ -33,7 +95,7 @@ static void clock_update_proc(Layer *layer, GContext *ctx) {
   // White clockface
   GPoint centre = GPoint(bounds.size.w/2, bounds.size.h/2);
   uint16_t radius = bounds.size.w/2;
-  graphics_context_set_fill_color(ctx, PBL_IF_COLOR_ELSE(GColorPastelYellow, GColorWhite));
+  graphics_context_set_fill_color(ctx, settings.ClockColour);
   graphics_fill_radial(ctx, bounds, GOvalScaleModeFitCircle, radius, 0, DEG_TO_TRIGANGLE(360));
 
   // Background
@@ -122,8 +184,8 @@ static void hands_update_proc(Layer *layer, GContext *ctx) {
 
   // dot in the middle
   uint32_t radius = bounds.size.w/2; 
-  graphics_context_set_fill_color(ctx, PBL_IF_COLOR_ELSE(GColorYellow, GColorBlack));
-  graphics_context_set_stroke_color(ctx, PBL_IF_COLOR_ELSE(GColorWindsorTan, GColorBlack));
+  graphics_context_set_fill_color(ctx, settings.HandColour);
+  graphics_context_set_stroke_color(ctx, settings.HandOutlineColour);
   GRect frame = grect_inset(bounds, GEdgeInsets(11*radius/12));
   graphics_fill_radial(ctx, frame, GOvalScaleModeFitCircle, radius, 0, DEG_TO_TRIGANGLE(360));
   graphics_draw_arc(ctx, frame, GOvalScaleModeFitCircle, 0, DEG_TO_TRIGANGLE(360));
@@ -146,6 +208,8 @@ static void prv_window_load(Window *window) {
   s_hands_layer = layer_create(bounds);
   layer_set_update_proc(s_hands_layer, hands_update_proc);
   layer_add_child(window_layer, s_hands_layer);
+
+  prv_update_display();
 }
 
 static void prv_window_unload(Window *window) {
@@ -158,6 +222,11 @@ static void prv_window_unload(Window *window) {
 }
 
 static void prv_init(void) {
+  prv_load_settings();
+
+  app_message_register_inbox_received(prv_inbox_received_handler);
+  app_message_open(128, 128);
+
   s_window = window_create();
   window_set_window_handlers(s_window, (WindowHandlers) {
     .load = prv_window_load,
